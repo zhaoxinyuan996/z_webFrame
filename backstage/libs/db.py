945b7edd56e1d9a56ab6cfc50fcd9169d29cad38
zxy <1178232101@qq.com>
2020-03-29 22:26:43 +0800

    部署，解决和django的冲突

--- backstage/libs/db.py
+++ backstage/libs/db.py
@@ -11,9 +11,9 @@ class Connection():
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.close()
 
-# connection = Connection()
-
 
 if __name__ == '__main__':
-    # print(connection.__enter__)
-    pass
+
+    with Connection() as cursor:
+        cursor.execute('show tables')
+        print(cursor.fetchall())
