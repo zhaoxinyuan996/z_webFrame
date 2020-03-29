7a0ad025cb46c179351c8fa707c3b2ae96adf13f
zxy <1178232101@qq.com>
2020-03-29 22:38:14 +0800

    部署，解决和django的冲突

--- backstage/views.py
+++ backstage/views.py
@@ -46,9 +46,11 @@ def limit(request):#分页 默认第一页 截取前40
     return httpResponse(json.dumps(dic))
 def page(request):#带page参数的分页，默认page=1
     get_ip(request,'page')
+    print(request.__dict__)
+    print('method', request.httpMethod)
     if request.httpMethod=='POST':
         # res=json.loads(request.body.decode())
-        print(request.__dict__)
+
         page=request['page']
         with Connection() as cursor:
             # cursor=connection.cursor()
