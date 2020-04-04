b12672e71d7be7070740995e09d15a2642e371da
zxy <1178232101@qq.com>
2020-04-04 23:41:18 +0800

    添加io多路复用引擎

--- backstage/multi_engine.py
+++ backstage/multi_engine.py
@@ -1,6 +1,6 @@
 import selectors
 from functools import partial
-from socket import socket, SOL_SOCKET, SO_REUSEADDR, gethostbyname, gethostname
+from socket import socket, SOL_SOCKET, SO_REUSEADDR, gethostbyname, gethostname, SOL_SOCKET, SO_SNDBUF
 
 # 接入
 from backstage.libs.handle import handle_url
@@ -10,6 +10,7 @@ from backstage.libs.static import httpRequest
 def accept(sel, s, seat):
     c, addr = s.accept()
     c.setblocking(False)
+    c.setsockopt(SOL_SOCKET, SO_SNDBUF, 819200000)
 
     sel.register(c, selectors.EVENT_READ, partial(read_func, sel, addr))
 
@@ -25,20 +26,14 @@ def read_func(sel, addr, c, seat):
         sel.unregister(c)
 
 # 写
-# lis = []
 def write_func(sel, request, c, seat):
     handle_url(c, request)
-    sel.modify(c, selectors.EVENT_READ, partial(read_func, sel, None))
-    # 非阻塞引起的bug
-    # if c not in lis:
-    #     print(c, '不在')
-    #     handle_url(c, request)
-    #     lis.append(c)
-    # else:
-    #     print(c, '在')
-    #     sel.unregister(c)
-    #     lis.remove(c)
+    sel.modify(c, selectors.EVENT_WRITE, partial(logout, sel, None))
 
+def logout(sel, request, c, seat):
+    print(id(c))
+    sel.unregister(c)
+    del c
 
 # 读为1 写为2
 def engine(sel, port):
