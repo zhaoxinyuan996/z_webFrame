18dd4ddf16f78eb9cbd12454a214972b046c6205
zxy <1178232101@qq.com>
2020-04-05 00:33:03 +0800

    添加io多路复用引擎

--- backstage/multi_engine.py
+++ backstage/multi_engine.py
@@ -10,17 +10,13 @@ from backstage.libs.static import httpRequest
 def accept(sel, s, seat):
     c, addr = s.accept()
     c.setblocking(False)
-    c.setsockopt(SOL_SOCKET, SO_SNDBUF, 819200000)
-
+    print(id(c))
     sel.register(c, selectors.EVENT_READ, partial(read_func, sel, addr))
 
 # 读
 def read_func(sel, addr, c, seat):
-    if not addr:
-        sel.unregister(c)
-        return
     try:
-        request = httpRequest(c.recv(1024), addr=addr[0])
+        request = httpRequest(c.recv(102400), addr=addr[0])
         sel.modify(c, selectors.EVENT_WRITE, partial(write_func, sel, request))
     except (ConnectionResetError, ConnectionAbortedError):
         sel.unregister(c)
@@ -28,12 +24,16 @@ def read_func(sel, addr, c, seat):
 # 写
 def write_func(sel, request, c, seat):
     handle_url(c, request)
+    # sel.unregister(c)
     sel.modify(c, selectors.EVENT_WRITE, partial(logout, sel, None))
-
+lis = []
 def logout(sel, request, c, seat):
-    print(id(c))
+    lis.append(c)
+    # for i in lis:
+    #     print(id(i), end=' ')
+#     # print(c)
     sel.unregister(c)
-    del c
+#     del c
 
 # 读为1 写为2
 def engine(sel, port):
@@ -41,7 +41,7 @@ def engine(sel, port):
     print('当前地址为%s:%s' % (serverIp, port))
 
     sock = socket()
-    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
     sock.setblocking(False)
     sock.bind(('0.0.0.0', port))
     sock.listen()
